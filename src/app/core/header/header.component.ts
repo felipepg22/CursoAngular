9c505275ad23976e977641bae626139a9480f90d
Felipe <fellippeg@hotmail.com>
2020-08-13 11:33:40 -0300

    Colocar o nome de usuário no header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,7 +1,22 @@
 import { Component } from '@angular/core';
 
+import { UserService } from '../user/user.service';
+import { Observable } from 'rxjs';
+import { User } from '../user/user';
+
 @Component({
     selector: 'ap-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+    private user$:Observable<User>;// $ -> Indica que vai receber um Observable
+    private user:User;
+
+    constructor(userService:UserService){
+
+        this.user$ = userService.getUser();
+        this.user$.subscribe(user => this.user = user);
+        
+    }
+}
