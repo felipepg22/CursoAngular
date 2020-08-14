6cd3517d581c70cb3ed67269af21939880858987
Felipe <fellippeg@hotmail.com>
2020-08-14 15:22:31 -0300

    Criação do validador assíncrono para verificar se nome de usuário já existe

---
+++ src/app/home/signup/user-not-taken.validator.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+import { debounceTime, switchMap, map, first } from 'rxjs/operators';
+
+import { SignUpService } from './signup.service';
+
+@Injectable({ providedIn: 'root' })
+export class UserNotTakenValidatorService {
+
+    constructor(private signUpService:SignUpService){}
+
+    checkUserNameTaken(){
+        
+        return (control:AbstractControl) => {
+
+            return control
+                        .valueChanges
+                        .pipe(debounceTime(300))
+                        .pipe(switchMap(userName=>{
+
+                            return this.signUpService.checkUserNameNotTaken(userName);
+                            
+                        }                           
+                            ))
+                        .pipe(map(isTaken => isTaken ? {userNameTaken:true}:null))
+                        .pipe(first());//Pega o primeiro valor para poder parar a operação
+        }
+    }
+}
