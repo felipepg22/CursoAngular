285e9eafbebd1cb6c612ed2dfc765c8cd677337d
Felipe <fellippeg@hotmail.com>
2020-08-18 15:20:04 -0300

    Criado o interceptador HTTP

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,12 +3,20 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './header/header.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RequestInterceptor } from './auth/request.interceptor';
 
 @NgModule({
     declarations: [ HeaderComponent ],
     exports: [ HeaderComponent ],
     imports: [ 
         CommonModule,
-        RouterModule ]
+        RouterModule ],
+    providers:[{
+
+        provide:HTTP_INTERCEPTORS,
+        useClass: RequestInterceptor,
+        multi: true
+    }]
 })
 export class CoreModule {}
