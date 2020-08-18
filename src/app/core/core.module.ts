0874b80c5fc292043b89aaeb9c937d57ee48531f
Felipe <fellippeg@hotmail.com>
2020-08-18 16:31:27 -0300

    Criado o rodapé para adicionar fotos

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { HeaderComponent } from './header/header.component';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RequestInterceptor } from './auth/request.interceptor';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-    declarations: [ HeaderComponent ],
-    exports: [ HeaderComponent ],
+    declarations: [ 
+        HeaderComponent,
+        FooterComponent
+    ],
+    exports: [ 
+        HeaderComponent,
+        FooterComponent
+    ],
     imports: [ 
         CommonModule,
         RouterModule ],
--- src/app/home/home.routing.module.ts
+++ src/app/home/home.routing.module.ts
@@ -22,6 +22,9 @@ const routes:Routes = [
         {path:'signup',
         component:SignUpComponent    
         },
+    ]
+    }
+]
 
    
 
