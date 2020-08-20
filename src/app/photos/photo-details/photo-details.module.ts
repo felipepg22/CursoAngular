168b0b134e03ed7e3375149f6d61405eac58d79e
Felipe <fellippeg@hotmail.com>
2020-08-20 16:34:37 -0300

    Criando o formulário e as validações dos comentários

--- src/app/photos/photo-details/photo-details.module.ts
+++ src/app/photos/photo-details/photo-details.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { PhotoDetailsComponent } from './photo-details.component';
 import { PhotoModule } from '../photo/photo.module';
 import { PhotoCommentsComponent } from './photo-comments/photo-comments.component';
+import { VMessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
 
 @NgModule({
 
@@ -19,7 +21,9 @@ import { PhotoCommentsComponent } from './photo-comments/photo-comments.componen
     imports:[
         CommonModule,
         PhotoModule,
-        RouterModule
+        RouterModule,
+        ReactiveFormsModule,
+        VMessageModule
     ]
 })
 export class PhotoDetailsModule {}
